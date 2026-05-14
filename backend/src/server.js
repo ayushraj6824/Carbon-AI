3c7ca8499b4b472a028f2aecb76cd92aefb0d227
Abhijeet Singh Rajput <abhijeet62008@gmail.com>
2026-05-14 16:41:53 +0530

    feat: Update environment variables and add production ML_SERVICE_URL

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,6 +17,7 @@ app.use(
       "http://localhost:3000",
       "http://localhost:3001",
       'https://neuralcarbon.vercel.app',
+      'http://neuralcarbon.vercel.app',
     ],
     credentials: true,
   }),
